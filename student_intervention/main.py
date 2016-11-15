7520f24f52f119885adc7039f82409ad9e506b6d
Jose Luis Uribe <jluribe@gmail.com>
2016-11-15 16:21:38 -0200

    update excel exercises

---
+++ student_intervention/main.py
@@ -0,0 +1,9 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from time import time
+from sklearn.metrics import f1_score
+
+# Read student data
+student_data = pd.read_csv("student-data.csv")
+print "Student data read successfully!"
