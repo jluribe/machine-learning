d4b37b1884e86451fb7b3616b8382bd43fbf3cf1
jluribe <jluribe@gmail.com>
2017-03-20 03:56:26 -0300

    Answer Question 6

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -77,7 +77,6 @@ class LearningAgent(Agent):
         # Calculate the maximum Q-value of all actions for a given state
         # maxQ = max(self.Q[state].values())
         maxQ = max(self.Q[state], key=self.Q[state].get)
-
         return maxQ 
 
 
@@ -185,14 +184,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, size=None, update_delay=0.01, display=False, log_metrics=True, optimized=False)
+    sim = Simulator(env, size=None, update_delay=0.01, display=True, log_metrics=True, optimized=False)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(tolerance=0.03, n_test=10)
+    sim.run(tolerance=0.05, n_test=10)
 
 
 if __name__ == '__main__':
